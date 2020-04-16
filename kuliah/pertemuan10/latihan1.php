5342268ce7be91e27b15b23470b4f7a06ba77740
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2020-04-16 16:59:28 -0700

    pertemuan10

--- kuliah/pertemuan10/latihan1.php
+++ kuliah/pertemuan10/latihan1.php
@@ -1,3 +1,24 @@
+<?php
+// Koneksi ke DB & Pilih  Database
+$conn = mysqli_connect('localhost', 'root', '', 'pw_193040021');
+
+// Query isi tabel mahasiswa
+$result = mysqli_query($conn, "SELECT * FROM mahasiswa");
+
+// ubah data kedalam array 
+//$row = mysqli_fetch_row($result); // array numerik
+//$row = mysqli_fetch_assoc($result); // array associativer
+//$row = mysqli_fetch_array($result); // keduanya
+
+while ($row = mysqli_fetch_assoc$result)) {
+  echo 
+}
+
+
+// tampung ke variabel mahasiswa
+
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,13 +36,14 @@
       <th>#</th>
       <th>Gambar</th>
       <th>NRP</th>
+      <th>Nama</th>
       <th>Email</th>
       <th>Jurusan</th>
       <th>Aksi</th>
     </tr>
     <tr>
       <td>1</td>
-      <td><img src="img/yuni.JPG"></td>
+      <td><img src="img/yuni.JPG" width="60"></td>
       <td>193040021</td>
       <td>Yuni Setyawati</td>
       <td>yunisetyawati@gmail.com</td>
