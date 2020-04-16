4a702b9680b3466003ff4d97ac980c288fccd8d1
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2020-04-16 15:19:42 -0700

    pertemuan10

---
+++ kuliah/pertemuan10/latihan1.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Daftar Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Daftar Mahasiswa</h3>
+
+  <table border="1" cellpadding="10" cellspacing="0">
+    <tr>
+      <th>#</th>
+      <th>Gambar</th>
+      <th>NRP</th>
+      <th>Email</th>
+      <th>Jurusan</th>
+      <th>Aksi</th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td><img src="img/yuni.JPG"></td>
+      <td>193040021</td>
+      <td>Yuni Setyawati</td>
+      <td>yunisetyawati@gmail.com</td>
+      <td>Teknik Informatika</td>
+      <td>
+        <a href="">ubah</a> | <a href="">hapus</a>
+      </td>
+    </tr>
+  </table>
+</body>
+
+</html>
