39574774aba79bd96679d08c2a564fd40899fcc7
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2020-04-26 10:48:30 -0700

    Tugas P6

---
+++ praktikum/P6_SENIN13_193040021/latihan6d/php/admin.php
@@ -0,0 +1,63 @@
+<?php
+require 'functions.php';
+
+$barang = query("SELECT * FROM elektronik");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin</title>
+  <style>
+    body {
+      background: linear-gradient(to right, #f05053 30%, #e1eec3);
+    }
+
+    img {
+      height: 130px;
+
+    }
+
+    table {
+      text-align: center;
+    }
+  </style>
+</head>
+
+<body>
+  <table border="1" cellpadding="13" cellspacing="1">
+    <tr>
+      <th>#</th>
+      <th>Opsi</th>
+      <th>Foto</th>
+      <th>Nama</th>
+      <th>Warna</th>
+      <th>Stok</th>
+      <th>Harga</th>
+    </tr>
+    <div>
+      <a href="ubah.php?id=<?= $e['id'] ?>" onclick="return confirm('Ubah Data??')">Ubah</a>
+      <a href="hapus.php?id=<?= $e['id'] ?>" onclick="return confirm('Hapus Data??')">Hapus</a>
+    </div>
+    <?php $i = 1; ?>
+    <?php foreach ($barang as $e) : ?>
+      <tr>
+        <td><?= $i; ?></td>
+        <td>
+          <a href=""><button>Ubah</button></a>
+          <a href="hapus"><button>Hapus</button></a>
+        </td>
+        <td><img src="../assets/img/<?= $e['foto']; ?>" </td> <td><?= $e['nama']; ?></td>
+        <td><?= $e['warna']; ?></td>
+        <td><?= $e['stok']; ?></td>
+        <td><?= $e['harga']; ?></td>
+      </tr>
+      <?php $i++; ?>
+    <?php endforeach; ?>
+  </table>
+</body>
+
+</html>
---
+++ praktikum/P6_SENIN13_193040021/latihan6d/php/tambah.php
@@ -0,0 +1,68 @@
+<?php 
+require 'functions.php';
+
+if (isset($_POST['tambah'])) {
+  if (tambah($_POST) > 0) {
+    echo "<script>
+                alert('Data Berhasil Ditambahkan!');
+                document.location.href = 'admin.php';
+          </script>";
+  } else {
+    echo "<script>
+                alert('Data Gagal Ditambahkan!');
+                document.location.href = 'admin.php';
+          </script>";
+  }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Form Tambah Data Mahasiswa</title>
+</head>
+
+<body>
+  <h3>Form Tambah Data</h3>
+  <form action="" method="POST">
+    <ul>
+      <li>
+        <label for="nama">Nama :</label><br>
+        <input type="text" name="nama" id="nama" required><br><br>
+      </li>
+      <li>
+        <label for="warna">Warna :</label><br>
+        <select name="warna" id="warna" required>
+          <option value="">----------- Pilih Warna -----------</option>
+          <option value="Black">Black</option>
+          <option value="White">White</option>
+          <option value="Blue">Blue</option>
+          <option value="Red">Red</option>
+          <option value="Gold">Gold</option>
+          <option value="Blue-Grey">Blue-Grey</option>
+          <option value="Grey">Grey</option>
+          <option value="Lightblue">Lightblue</option>
+          <option value="Dark-Blue">Dark-Blue</option>
+          <option value="Pink-White">Pink-White</option>
+        </select>
+      </li>
+      <li>
+        <label for="stok">Stok :</label><br>
+        <input type="text" name="stok" id="stok" required><br><br>
+      </li>
+      <li>
+        <label for="harga">Harga :</label><br>
+        <input type="text" name="harga" id="harga" required><br><br>
+      </li>
+      <br>
+      <button type="submit" name="tambah">Tambah Data!</button>
+      <button type="submit">
+        <a href="index.php" style="text-decoration: none; color: black;">Kembali</a>
+      </button>
+    </ul>
+  </form>
+</body>
+</html>
