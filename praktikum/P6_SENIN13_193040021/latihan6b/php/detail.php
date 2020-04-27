b54177102f8f6e33a7c370a70f896718ac09ae54
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2020-04-27 12:24:38 -0700

    P6

--- praktikum/P6_SENIN13_193040021/latihan6b/php/detail.php
+++ praktikum/P6_SENIN13_193040021/latihan6b/php/detail.php
@@ -16,7 +16,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>detail 5c</title>
+    <title>detail 6c</title>
     <style>
         .container {
             width : 300px;
