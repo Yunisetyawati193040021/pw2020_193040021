778454a8c13edcbe87a9704b978bf4e220aa383c
Yunisetyawati30 <60532944+Yunisetyawati30@users.noreply.github.com>
2020-04-29 11:41:24 -0700

    TUGAS PRAKTIKUM MODUL 6

--- praktikum/P6_SENIN13_193040021/latihan6d/php/detail.php
+++ praktikum/P6_SENIN13_193040021/latihan6d/php/detail.php
@@ -1,57 +1,60 @@
 <?php
 
-    if(!isset($GET['id'])) {
-        header("location: ../index.php");
-    }
+require 'functions.php';
 
-    require 'functions.php';
+$id = $_GET['id'];
 
-    $id = $_GET['id'];
-
-    $barang = query("SELECT * FROM elektronik WHERE id = $id")[0];
+$barang = query("SELECT * FROM elektronik WHERE id = $id")[0];
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>detail 6d</title>
     <style>
         .container {
-            width : 300px;
+            width: 300px;
             height: 520px;
-            margin : auto;
-            border : 50px solid	#845539; 
+            margin: auto;
+            border: 50px solid #845539;
             background-color: #CC9966;
-            padding : 20px 20px 20px 20px;
-            font-family : arial;
+            padding: 20px 20px 20px 20px;
+            font-family: arial;
             font-size: 20px;
         }
+
         a:link {
-            color : #000000;
+            color: #000000;
         }
+
         a:hover {
             text-decoration: none;
-            color : #D2691E;
+            color: #D2691E;
         }
+
         a:active {
             text-decoration: none;
         }
     </style>
 </head>
+
 <body>
     <div class="container">
-        <div class="foto">
-            <img src="../assets/img/<?=$e["foto"]; ?>" alt="">
-        </div>
-        <div class="keterangan">
-            <p><?=$e["nama"]; ?></p>
-            <p><?=$e["warna"]; ?></p>
-            <p><?=$e["stok"]; ?></p>
-            <p><?=$e["harga"]; ?></p>
-        </div>
-        <button class="tombol-kembali"><a href="index.php">Kembali</button>
+            <div class="foto">
+                <ul>
+                    <li><img src="assets/img/<?= $e['foto']; ?>" alt=""></li>
+                    <li>Nama : <?= $e['nama']; ?></li>
+                    <li>Warna : <?= $e['warna']; ?></li>
+                    <li>Stok : <?= $e['stok']; ?></li>
+                    <li>Harga : <?= $e['harga']; ?></li>
+                </ul>
+
+            </div>
+        <button class="tombol-kembali"><a href="../index.php">Kembali</button>
     </div>
 </body>
+
 </html>
